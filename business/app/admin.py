b4d0f359a8180f087666d1a616957e28c8d3692f
Filipp Zhuravlev <shiningfinger@list.ru>
2017-10-17 16:56:19 +0300

    I've created user and share models with their visibility in the site's admin panel.

--- business/app/admin.py
+++ business/app/admin.py
@@ -1,4 +1,35 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
+from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('last_name', 'first_name', 'middle_name', 'consultant_num', 'phone_number', 'email', 'status',)
+    date_hierarchy = 'registered_date'
+    filter_fields = ('registered_date', 'status', 'last_name', 'first_name', 'middle_name', 'citizenship', 'city', 'region',)
+    search_fields = (
+        'last_name',
+        'first_name',
+        'middle_name',
+        'city',
+        'region',
+        'consultant_num',
+        'passport_data',
+        'birthday',
+        'street',
+        'num_home',
+        'num_apartment',
+        'email',
+        'phone_number',
+    )
+
+class ShareAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('title', 'published_date',)
+    date_hierarchy = 'published_date'
+    filter_fields = ('published_date', 'title',)
+    search_fields = ('published_date', 'title',)
 
 # Register your models here.
+admin.site.register(Share, ShareAdmin)
+admin.site.register(User, UserAdmin)
--- business/app/models.py
+++ business/app/models.py
@@ -2,11 +2,93 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 
-# if not settings.DEBUG:
-#     encoding = 'utf-8'
-#     import sys
-#     reload(sys)
-#     sys.setdefaultencoding(encoding)
+if not settings.DEBUG:
+    encoding = 'utf-8'
+    import sys
+    reload(sys)
+    sys.setdefaultencoding(encoding)
 
-# Create your models here.
+
+
+class Share(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Название акции")
+
+    text = models.TextField(
+        _('Дополнительный текст'),
+        max_length=16384, blank=True, null=True)
+    created_date = models.DateTimeField(
+        _('Дата создания'),
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        _('Дата публикации'),
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = _('Акция')
+        verbose_name_plural = _('Акции')
+        ordering = ['-published_date']
+
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+
+    last_name = models.CharField(verbose_name='Фамилия', max_length=36)
+    first_name = models.CharField(verbose_name='Имя', max_length=32)
+    middle_name = models.CharField(verbose_name='Отчество', max_length=32, blank=True, null=True)
+
+    birthday = models.DateTimeField(verbose_name='День рождения')
+    citizenship = models.BooleanField(default=False, verbose_name='Не гражданин РФ.')
+    passport_data = models.CharField(max_length=26, verbose_name='Серия и номер паспорта')
+    phone_number = models.CharField(max_length=26, verbose_name='Номер телефона')
+
+    city = models.CharField(verbose_name='Город', max_length=50)
+    region = models.CharField(verbose_name='Область', max_length=50, blank=True, null=True)
+
+    street = models.CharField(verbose_name='Улица', max_length=50)
+    num_home = models.CharField(verbose_name='Дом' , max_length=5)
+    num_apartment = models.DecimalField(verbose_name='Квартира', max_digits=999, decimal_places=1)
+
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+
+    consultant_num = models.CharField(verbose_name='Номер консультанта', max_length=40, blank=True, null=True)
+
+    register_statuses = (
+        (_('Новый'), 'Новый'),
+        (_('Пустой'), 'Пустой'),
+	(_('Зерегистрированный А'), 'Зерегистрированный А'),
+	(_('Зерегистрированный Б'), 'Зерегистрированный Б'),
+    )
+
+    status = models.CharField(
+        max_length=24,
+        choices=register_statuses,
+        default='Новый',
+        verbose_name='Статус регистрации'
+    )
+
+    registered_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Консультант')
+        verbose_name_plural = _('Консультанты')
+        ordering = ['-registered_date']
+
+    def register(self):
+        self.registered_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return '%s %s %s | Статус: %s' % (self.last_name, self.first_name, self.middle_name, self.status)
+
+
